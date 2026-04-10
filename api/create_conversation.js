081ccd6c382f97c470f5a55fb6f404f9e6332800
Brian Tabios <105179017+Brian-304@users.noreply.github.com>
2026-04-10 12:09:09 -0700

    Update replica_id in create_conversation.js

--- api/create_conversation.js
+++ api/create_conversation.js
@@ -14,8 +14,7 @@ export default async function handler(req, res) {
 
     // 3. The Payload - In v2, the key MUST be 'replica_id'
     const payload = {
-      replica_id: process.env.AVATAR_ID,
-      input: process.env.AVATAR_ID.trim()
+      replica_id: "r1aea373d24c"
     };
 
     console.log("Calling endpoint:", url);
